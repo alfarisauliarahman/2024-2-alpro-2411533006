6cb80565c2c3331e0cc3095d3d24d61ff7abad60
Alfaris Aulia Rahman <alfarisauliarahman@gmail.com>
2024-11-21 10:42:30 +0700

    Praktikum Pekan 6

---
+++ prakalpro_2_2411544006/src/pekan6/SentinelLoop.java
@@ -0,0 +1,20 @@
+package pekan6;
+
+import java.util.Scanner;
+
+public class SentinelLoop {
+
+	public static void main(String[] args) {
+		Scanner console = new Scanner (System.in);
+		int sum = 0;
+		int number = 12;	//"dummy value", anything but 0
+		
+		while (number !=0) {
+			System.out.print("Masukkan angka (0 untuk keluar): ");
+			number = console.nextInt();
+			sum = sum + number;
+		}
+		System.out.println("Totalnya adalah " + sum);
+	}
+
+}
